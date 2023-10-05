9b7ecc3a55f9f9b9e6c05455a54d9a34f6768ef2
Dani <daniyil.kud@gmail.com>
2023-10-05 05:51:01 +0200

    ads

---
+++ src/app/components/header-card/header-card.component.ts
@@ -0,0 +1,9 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+    selector: 'app-header-card',
+    templateUrl: './header-card.component.html',
+    styleUrls: ['./header-card.component.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+export class HeaderCardComponent {}
